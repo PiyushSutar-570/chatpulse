ae8f75c9a9bcb23500ace93bb01b9f7181afa21e
PiyushSutar-570 <piyushsutar570@gmail.com>
2026-04-22 13:53:56 +0530

    making the app production ready for deployment

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -2,5 +2,12 @@ import express from 'express'
 
 const router = express.Router();
 
+router.get("/signup",(req,res)=>{
+    res.send("Sign up end point !");
+})
+
+router.get("/login",(req,res)=>{
+    res.send("Login end point !");
+})
 
 export default router;
