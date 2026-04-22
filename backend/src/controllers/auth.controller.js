a1f505b99e01889b0314f62a3fe186db791c7cdf
PiyushSutar-570 <piyushsutar570@gmail.com>
2026-04-22 20:41:06 +0530

    sign up controller added

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,63 @@
+import { generateToken } from '../lib/utils.js';
+import User from '../models/User.js'
+import bcrypt from 'bcryptjs'
+
+export const signup = async(req,res)=>{
+
+    const {fullName,email,password} = req.body;
+
+    try{
+
+        if(!fullName || !email || !password){
+            return res.status(400).json({
+                message : 'All fields are required !'
+            })
+        }
+
+        if(password.length<6){
+            return res.status(400).json({
+                message : "Password must be atleast 6 characters long !"
+            })
+        }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!emailRegex.test(email)){
+            return res.status(400).json({
+                message : 'Invalid Email !'
+            })
+        }
+
+        const user = await User.find({email});
+        if(user){
+            return res.status(400).json({
+                message : 'User already exists !'
+            })
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt);
+
+        const newUser = await User({
+            fullName,
+            email,
+            password : hashedPassword,
+        });
+
+        if(newUser){
+            const savedUser = await newUser.save();
+            generateToken(savedUser._id, res);
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
+            });
+        }
+    }
+    catch(error){
+        console.log('Error in sign-up controller',error);
+        res.status(500).json({
+            message : "Internal server error"
+        })
+    }
+};
---
+++ backend/src/lib/utils.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken'
+
+export const generateToken = async(userId,res)=>{
+    const JWT_SECRET = process.env.JWT_SECRET
+
+    if(!JWT_SECRET){
+        throw new Error("Jwt secret is not configured !");
+    }
+
+    const token = jwt.sign({userId},JWT_SECRET,{
+        expiresIn : '7d'
+    });
+
+    res.cookie("jwt",token,{
+        maxAge : 7*24*60*60*1000,
+        httpOnly : true,
+        sameSite : "strict",
+        secure : process.env.NODE_ENV === "development" ? false : true
+    });
+
+    return token;
+};
